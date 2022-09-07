2149904352f3dcf97bb42c26f6b213ea324612d5
NatalyM <nbelokopytova@gmail.com>
2022-09-07 13:08:14 +0300

    Hello from Natalia

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,8 +1,17 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
     @Test
-    public void helloWord() {
-        System.out.println("Hello World");
+    public void helloFrom() {
+        System.out.println("Hello from Natalia");
+    }
+    @Test
+    public void firstTestAPI() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/hello")
+                .andReturn();
+        response.prettyPrint();
     }
 }

747232be7f0195b1f1c642e799c53f0a6c8faf8f
NatalyM <nbelokopytova@gmail.com>
2022-09-15 09:14:43 +0300

    Ex6:* Редирект

--- src/test/java/Homework2Test.java
+++ src/test/java/Homework2Test.java
@@ -1,10 +1,16 @@
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class Homework2Test {
+    /***
+     * Ex5: Парсинг JSON
+     * Сделать GET-запрос.
+     * Вывести текст второго сообщения.
+      */
     @Test
-    public void getAnyParamTest(){
+    public void ParsingJsonTest(){
         JsonPath response = RestAssured
                 .get("https://playground.learnqa.ru/api/get_json_homework")
                 .jsonPath();
@@ -12,4 +18,23 @@ public class Homework2Test {
         String message = response.get("messages.message[1]");
         System.out.println(message);
     }
+
+    /***
+     * Ex6:* Редирект
+     * Сделать GET-запрос.
+     * Распечатать адрес, на который редиректит указанные URL.
+     */
+    @Test
+    public void RedirectTest(){
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+
+        System.out.println(response.getHeader("Location"));
+    }
+
+
 }

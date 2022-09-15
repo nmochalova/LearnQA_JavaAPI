1f2dc916b57020139777c95d3f283e2a61dea3e8
NatalyM <nbelokopytova@gmail.com>
2022-09-15 09:29:18 +0300

    Ex7:* Долгий редирект

--- src/test/java/Homework2Test.java
+++ src/test/java/Homework2Test.java
@@ -36,5 +36,29 @@ public class Homework2Test {
         System.out.println(response.getHeader("Location"));
     }
 
+    /***
+     * Ex7:* Долгий редирект
+     * Сделать GET-запрос.
+     * Написать цикл, который будет создавать запросы в цикле, каждый раз читая URL для редиректа из нужного заголовка.
+     * И так, пока мы не дойдем до ответа с кодом 200.
+     */
+    @Test
+    public void LongRedirectTest(){
+        String url = "https://playground.learnqa.ru/api/long_redirect";
+        int statusCode = 0;
+
+        while (statusCode != 200) {
+            Response response = RestAssured
+                    .given()
+                    .redirects()
+                    .follow(false)
+                    .get(url)
+                    .andReturn();
+
+            statusCode = response.getStatusCode();
+            url = response.getHeader("Location");
+            System.out.println(String.format("Status code: %d, URL: %s", statusCode, url));
+        }
+    }
 
 }
